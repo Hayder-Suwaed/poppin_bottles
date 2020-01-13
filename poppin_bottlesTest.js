70199d97abd32c50d2152b756df4d2780f4342fc
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-13 02:23:22 +0000

    Problem Solving - Poppin Bottles

---
+++ poppin_bottlesTest.js
@@ -0,0 +1,10 @@
+var assert = require("chai").assert;
+var poppingBottles = require("../poppingBottles");
+
+describe("Popping Bottles", function() {
+  it("It Should return a string with all poppingBottles information.", function() {
+    var cash = 40;
+    var result = poppingBottles(cash);
+    assert.isString(result);
+  });
+});
